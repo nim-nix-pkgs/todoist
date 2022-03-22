8c3589c5e68928e5ee66aaa9425c01a6a15ce716
hugosenari <hugosenari@gmail.com>
2022-03-22 00:38:36 -0300

    chore: re-index ../nimpkgs/t/todoist

--- v0_1_1/flake.nix
+++ v0_1_1/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-todoist-v0_1_1.flake = false;
   inputs.src-todoist-v0_1_1.owner = "ruivieira";
-  inputs.src-todoist-v0_1_1.ref   = "refs/tags/v0.1.1";
+  inputs.src-todoist-v0_1_1.ref   = "v0_1_1";
   inputs.src-todoist-v0_1_1.repo  = "nim-todoist";
   inputs.src-todoist-v0_1_1.type  = "github";
   
